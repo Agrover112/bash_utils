246c07be49940531fffb70b020519f70defbf752
Saumit Dinesan <justsaumit@protonmail.com>
2022-10-03 10:22:19 +0530

    Aliases cannot use positional arguments just add arguments after the alias and they would work

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -6,21 +6,21 @@ alias seder='sed  's/bin\/activate$//' '  # Cannot remember
 #alias python=python2.7 
 
 # Conda Aliases                                                                  
-alias conda-remove-env='conda env remove --name $1'                             
-alias conda-create-env='conda create --name $1'                                 
-alias conda-lsp='conda list --name $1' # List conda packages                                          
-alias conda-ls='conda env list'        # List conda environments                                                  
+alias conda-remove-env='conda env remove --name'
+alias conda-create-env='conda create --name'
+alias conda-lsp='conda list --name' # List conda packages                                          
+alias conda-ls='conda env list'     # List conda environments                                                  
                                                                                  
 #Git Aliases                                                                     
 alias git-graph='git log --graph --oneline'                      # A simpler heuristic                                   
-alias git-show-lastn=' bash /home/meow/scripts/gitshowcom.sh $1' # Look behind at the nth commit back in the log.
+alias git-show-lastn=' bash /home/meow/scripts/gitshowcom.sh' # Look behind at the nth commit back in the log.
 
 # Utils
 function lsize() { du -s ${1}/* | sort -rn | cut -f2- | xargs -d "\n" du -sh }
-alias ls-size='lsize $1wq'
+#alias ls-size='lsize $1wq'
 alias ls-hidden='ls -d .* --color=auto'  # List hidden files
 
-alias speedread='~/$USER/Downloads/speedread/speedread $1'
+alias speedread='~/$USER/Downloads/speedread/speedread'
 
 function bat-help(){ $1 --help | batcat }  #Bat is requird
-alias bathelp='bat-help $1'
+alias bathelp='bat-help'
