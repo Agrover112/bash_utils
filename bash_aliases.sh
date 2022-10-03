b51246745930fc389aa1a87ec1e20958bd374179
Saumit Dinesan <justsaumit@protonmail.com>
2022-10-03 11:15:25 +0530

    git aliases for #2

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -14,8 +14,9 @@ alias conda-lsp='conda list --name'                 # List conda packages
 alias conda-i="conda install"                       # Installs a list of packages into a specified conda environment provided as arguments
 alias conda-r="conda remove"                        # Removes a list of packages from a specified conda environment provided as arguments
 
-#Git Aliases                                                                     
-alias git-graph='git log --graph --oneline'                      # A simpler heuristic                                   
+#Git Aliases                                                
+
+alias git-graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''%C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all" # Simple and Beautiful way to display git log
 alias git-show-lastn=' bash $HOME/scripts/gitshowcom.sh' # Look behind at the nth commit back in the log.
 
 # Utils
@@ -23,7 +24,7 @@ lsize() {
         du -s "${1}"/* 2> >(grep -v '^du: cannot \(access\|read\)' >&2) | sort -rn | \
         cut -f2- | xargs -d "\n" du -sh 2> >(grep -v '^du: cannot \(access\|read\)' >&2);
 }
-#alias ls-size='lsize $1wq'
+
 alias ls-hidden='ls -d .* --color=auto'  # List hidden files
 
 alias speedread='$HOME/Downloads/speedread/speedread'
