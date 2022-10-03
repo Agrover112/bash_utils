68da8b073993c7fce39eca9af9e732d8d3bbfe54
Saumit Dinesan <justsaumit@protonmail.com>
2022-10-03 11:06:20 +0530

    conda aliases #2

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # Venv ALiases                                                                   
-alias venv-list='locate --regex "bin/activate"$'        # Locate /bin/activate paths for both conda and venv                         
-alias seder='sed  's/bin\/activate$//' '  # Cannot remember
+alias venv-list='locate --regex "bin/activate"$'    # Locate /bin/activate paths for both conda and venv                         
+alias seder='sed  's/bin\/activate$//' '            # Cannot remember
 
 #alias python=python2.7 
 
 # Conda Aliases                                                                  
-alias conda-remove-env='conda env remove --name'
-alias conda-create-env='conda create --name'
-alias conda-lsp='conda list --name' # List conda packages                                          
-alias conda-ls='conda env list'     # List conda environments                                                  
-                                                                                 
+alias conda-create-env='conda env create --name'    # Creates a conda enviroment with the name as first argument($1)
+alias conda-remove-env='conda env remove --name'    # Removes a conda enviroment with the name as first argument($1)
+alias conda-eu="conda env update"                   # Updates the current conda environment
+alias conda-ls='conda env list'                     # List conda environments
+alias conda-lsp='conda list --name'                 # List conda packages                                          
+alias conda-i="conda install"                       # Installs a list of packages into a specified conda environment provided as arguments
+alias conda-r="conda remove"                        # Removes a list of packages from a specified conda environment provided as arguments
+
 #Git Aliases                                                                     
 alias git-graph='git log --graph --oneline'                      # A simpler heuristic                                   
 alias git-show-lastn=' bash $HOME/scripts/gitshowcom.sh' # Look behind at the nth commit back in the log.
